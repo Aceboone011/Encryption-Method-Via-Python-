2d99dffefbaba8a02d26ffaad83cd07f8b2c89d3
X.Scott.B <91287447+Aceboone011@users.noreply.github.com>
2023-07-28 12:30:57 -0400

    Update Encryption.py

--- Encryption.py
+++ Encryption.py
@@ -1,179 +1,99 @@
 import random
 
+#open and read file named 'SampleText.txt'
+#establish a list of strings containing ALL characters
+#ogdict == Original Dictionary
 
-plainText = open('text2.txt', 'r')
 
+plainText = open('SampleText.txt', 'r')
 ogdict = [
-
     'A', 'a', 'B', 'b', 'C', 'c', 'D', 'd', 'E', 'e', 'F', 'f', 'G', 'g', 'H',
-
     'h', 'I', 'i', 'J', 'j', 'K', 'k', 'L', 'l', 'M', 'm', 'N', 'n', 'O', 'o',
-
     'P', 'p', 'Q', 'q', 'R', 'r', 'S', 's', 'T', 't', 'U', 'u', 'V', 'v', 'W',
-
     'w', 'X', 'x', 'Y', 'y', 'Z', 'z', '1', '2', '3', '4', '5', '6', '7', '8',
-
     '9', '0', ' ', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_',
-
     '+', '=', '{', '}', '[', ']', '|', ':', ';', '"', '<', ',', '.', '>', '?',
+    '\n', '\t', '\'', '\r', '\v', '\f', '–', '’', 'ł', 'í', '¿', 'é']
 
-    '\n', '\t', '\'', '\r', '\v', '\f', '–', '’', 'ł', 'í', '¿', 'é'
-
-]
-
-
-
+# define encryption function
 def encrypt2(plainText):
-
-    encryption = " "
-
+#empty string
+    encryption = ""
     f = open('encryption2.txt', 'w')
 
+#create and write in new file
     for line in plainText:
-
         for a in line:
-
             cindex = ogdict.index(a)
-
             if cindex is not False:
-
-                cindex = ogdict.index(a)
-
+#The character at the corresponding index is now written in the encryption string
+#write newdict[#]
                 encryption = encryption + newdict[cindex]
-
-                #ogdict index value = newdict position
-
-                #write newdict[#]
-
+# If a character is not in the original dictionary(ogdict)then, the character is converted to a string representation, regardless of its original data type.
             elif cindex is not True:
-
                 a = str(a)
-
                 ogdict.extend(a)
-
+#add the new character to the original dictionary                
                 cindex = ogdict.index(a)
-
-                encryption = encryption + ogdict[cindex]  #if not in ogdict
-
+                encryption = encryption + ogdict[cindex]
+#if not in ogdict
         w = f.write(encryption)
-
-    #print(y,'little monkeys jumping off the bed')
-
     f.close
-
-
-
 plainText.close
 
-ogdict = [
-
-    'A', 'a', 'B', 'b', 'C', 'c', 'D', 'd', 'E', 'e', 'F', 'f', 'G', 'g', 'H',
-
-    'h', 'I', 'i', 'J', 'j', 'K', 'k', 'L', 'l', 'M', 'm', 'N', 'n', 'O', 'o',
-
-    'P', 'p', 'Q', 'q', 'R', 'r', 'S', 's', 'T', 't', 'U', 'u', 'V', 'v', 'W',
-
-    'w', 'X', 'x', 'Y', 'y', 'Z', 'z', '1', '2', '3', '4', '5', '6', '7', '8',
-
-    '9', '0', ' ', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_',
-
-    '+', '=', '{', '}', '[', ']', '|', ':', ';', '"', '<', ',', '.', '>', '?',
-
-    '\n', '\t', '\'', '\r', '\v', '\f', '–', '’', 'ł', 'í', '¿', 'é'
-
-]
 
 
+#Method to choose a number at random: get the amount of character in ogdict and choose a number at random
 x = len(ogdict)
-
 y = random.randrange(0, x)
-
+#shift index and reassign new dictionary to new variable: newdict
 newdict = ogdict[y:] + ogdict[:y]
-
-#print(newdict)
-
 newdict2 = newdict[-y:] + newdict[:-y]
-
-#print(newdict2)
-
-#print (x)
-
-#print(y)
-
 z = str(y)
+#convert random number chose to string data type
 
-zin = 0
 
 #decryption measure
-
 encrypt2(plainText)
 
+#read the string in z and conver that to a predetermined code that is produced alongside the encrypted messege
 
 for line in z:
-
     for num in line:
-
         f2 = open('encryption2.txt', 'a')
-
         axe = []
-
         if num == '1':
-
             axe += 'AA'
 
-
         elif num == '2':
-
             axe += 'AB'
 
-
         elif num == '3':
-
             axe += 'AT'
 
-
         elif num == '4':
-
             axe += 'AC'
 
-
         elif num == '5':
-
             axe += 'AD'
 
-
         elif num == '6':
-
             axe += 'AE'
 
-
         elif num == '7':
-
             axe += 'AG'
 
-
         elif num == '8':
-
             axe += 'AF'
-
+            
         elif num == '9':
-
             axe += 'AU'
-
+            
         else:
-
             axe += 'AP'
 
-
     h = '\n'
-
     f2.write(h)
-
     g = '\n'.join(map(str, axe))
-
     f2.write(g)
-
     f2.close()
-
-
-#Written using the “repl.it” Python 3 IDE.
